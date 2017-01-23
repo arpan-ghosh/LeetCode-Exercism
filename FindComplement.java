8cd9a752d908670382824af2a230d0a32649128e
Arpan Ghosh <arpanghosh95@gmail.com>
2017-01-22 23:19:46 -0500

    Given a positive integer, output its complement number.
    The complement strategy is to flip the bits of its binary representation.
    
    This version of my code is a slower version of the fastest way to solve this problem (most efficient solution takes one line). That can be found on my gists page.

---
+++ FindComplement.java
@@ -0,0 +1,40 @@
+
+public class FindComplement {
+	
+
+    public static int findComplement(int num) {
+    	
+    	int numComplement = 0;
+    	final int NEGATIVE = -1;
+    	
+    	String complement = Integer.toString(num,2);
+    	
+    	StringBuilder str = new StringBuilder(complement);
+
+    	
+    	for (int i = 0; i < str.length(); i++) {
+    		if (str.charAt(i) == '0') {
+    			str.setCharAt(i, '1');
+    		} else {
+    			str.setCharAt(i, '0');
+    		}
+    	}
+    	
+    	String comp = str.toString();
+    	   
+    	if (num < 0) {
+    		numComplement = Integer.parseInt(comp,2) * NEGATIVE;
+    	} else {
+    		numComplement = Integer.parseInt(comp, 2);
+    	}
+    	
+		return numComplement;
+    	
+    }
+    
+    public static void main(String[] args) {
+    	System.out.print(findComplement(-8));
+    }
+
+
+}
