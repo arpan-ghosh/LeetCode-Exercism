a72672955d6f3fb82360307d5422db47d1295d34
Arpan Ghosh <arpanghosh95@gmail.com>
2017-01-24 00:01:32 -0500

    LeetCode Exercise: FizzBuzz
    But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
    
    https://leetcode.com/problems/fizz-buzz/

---
+++ FizzBuzz.java
@@ -0,0 +1,44 @@
+/*
+ * Write a program that outputs the string 
+ * representation of numbers from 1 to n.
+ * But for multiples of three it should output “Fizz” 
+ * instead of the number and for the multiples of five output “Buzz”.
+ * For numbers which are multiples of both three and five output “FizzBuzz”.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+
+	    private static List<String> list = new ArrayList<String>();
+	    private static String numString = "";
+	    private static int count = 1;
+	    	    
+		public static List<String> fizzBuzz(int n) {
+			
+			
+			for (int i = n; i > 0; i--) {
+	    		numString = String.valueOf(count);
+	    		if ((count % 3 == 0) && (count % 5 == 0)) {
+	    			list.add("FizzBuzz");
+	    		} else if (count % 3 == 0) {
+	    			list.add("Fizz");
+	    		} else if (count % 5 == 0) {
+	    			list.add("Buzz");
+	    		} else {
+	    			list.add(numString);
+	    		}
+	    		count++;
+			}
+			
+			return list;
+	    		        
+	    }
+		    
+	    public static void main (String[] args) {
+	    	fizzBuzz(15);
+	    	list.stream().forEach(System.out::println);
+	    }
+	
+}
