0f958aa815dc73122e798bf17d6adbd3fb53fd59
Arpan Ghosh <arpanghosh95@gmail.com>
2017-06-16 21:43:01 -0400

    merge two sorted arrays and find its median

--- MedianSortedArrays.java
+++ MedianSortedArrays.java
@@ -43,10 +43,11 @@ public class MedianSortedArrays {
 		}
     }
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		int[] nums1 = {1,2,3,4,5};
 		int[] nums2 = {6,7,8,9,10}; 
 		System.out.print(findMedianSortedArrays(nums1, nums2));
 	}
+	*/
 
 }
