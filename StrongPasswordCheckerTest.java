634548a3e247cd26f6eb2873a7f6cb026dd287cb
Arpan Ghosh <arpanghosh95@gmail.com>
2017-06-19 23:21:41 -0400

    Password Test cases

--- StrongPasswordCheckerTest.java
+++ StrongPasswordCheckerTest.java
@@ -7,15 +7,31 @@ public class StrongPasswordCheckerTest extends StrongPasswordChecker {
 	@Test
 	public void testLength() {
 		assertEquals(strongPasswordChecker("abcd"),4);
-		assertEquals(strongPasswordChecker("abcdefghijlmnopqurst"), 3);
-		assertEquals(strongPasswordChecker("abcdefghijklmnopqrstu"), 4);
 	}
 	
 	@Test
+	public void testLength2() {
+		assertEquals(strongPasswordChecker("abcdefghijlmnopqurst"), 3);
+	}
+	
+	@Test
+	public void testLength3() {
+		assertEquals(strongPasswordChecker("abcdefghijklmnopqrstu"), 4);
+	}
+			
+	@Test
 	public void testLetters() {
-		assertEquals(strongPasswordChecker("Password*"), 2);
-		assertEquals(strongPasswordChecker("Password"), 4);
+		assertEquals(strongPasswordChecker("Password*"), 1);
+	}
+	
+	@Test
+	public void testLetters2() {
+		assertEquals(strongPasswordChecker("Password"), 2);
+	}
+	
+	@Test
+	public void testLetters3() {
 		assertEquals(strongPasswordChecker("Passworddd"),3);
 	}
-
+	
 }
