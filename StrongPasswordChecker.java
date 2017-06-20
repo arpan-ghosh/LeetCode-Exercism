797012273dcc5afeb9d1fe9cf21a42dfc9c5bb6a
Arpan Ghosh <arpanghosh95@gmail.com>
2017-06-19 23:30:20 -0400

    A password is considered strong if the following conditions are all met

--- StrongPasswordChecker.java
+++ StrongPasswordChecker.java
@@ -24,49 +24,46 @@ public class StrongPasswordChecker {
 	
 	public int strongPasswordChecker(String s) {
 
-		System.out.println(s + " " + s.length());
+		System.out.println("Your password is " + s + " with length " + s.length());
 		
 		
 		if (s.length() > 20 || s.length() < 6) {
-			System.out.println(s + " does not meet length criteria. counter = " + counter);
+			System.out.println(s + " does not meet length criteria");
 			counter++; 
 		}		
 		
 		if (s.length() <= 20 && s.length() >= 6) {
-			System.out.println(s + " meets length criteria. counter = " + counter);
+			System.out.println(s + " meets length criteria");
 			length = true;
 		}
 			
 		if (s.matches(".*\\d.*")) {
 			hasDigit = true;
-			System.out.println(s + " has a digit! counter = " + counter);
+			System.out.println(s + " has a digit");
 		} else {
-			System.out.println(s + " does not have a digit! counter = " + counter);
+			System.out.println(s + " does not have a digit");
 			counter++;
 		}
 		if (s.matches(".*[A-Z].*")) {
-			System.out.println(s + " has an uppercase letter! counter = " + counter);
+			System.out.println(s + " has an uppercase letter");
 			hasUppercase = true;
 		} else {
-			System.out.println(s + " does not have an uppercase letter! counter = " + counter);
+			System.out.println(s + " does not have an uppercase letter");
 			counter++;
 		}
 		if (s.matches(".*[a-z].*")) {
-			System.out.println(s + " has a lowercase letter. counter = " + counter);
+			System.out.println(s + " has a lowercase letter");
 			hasLowercase = true;
 		} else {
-			System.out.println(s + " does not have a lowercase letter! counter = " + counter);
+			System.out.println(s + " does not have a lowercase letter");
 			counter++;
 		}
 		if (s.matches(".*[[$&+,:;=?@#|'<>.^*()%!-]].*")) {
-			System.out.println(s + " has a special character! counter = " + counter);
+			System.out.println(s + " has a special character");
 			hasSpecial = true;
 		} else {
-			System.out.println(s + " does not have a special character! counter = " + counter);
+			System.out.println(s + " does not have a special character");
 			counter++;
-		}
-		
-		if (hasLowercase && hasUppercase && hasDigit && hasSpecial && noThreeRepeat) {
 		}
 		
 		
@@ -88,11 +85,21 @@ public class StrongPasswordChecker {
 		}
 		
 		if (flag) {
-			System.out.println(s + " has letters repeating thrice (flag). counter = " + counter);
+			System.out.println(s + " has letters repeating thrice");
 			counter++;
 		}
-		System.out.println(s + " counter is: " + counter);
+		
+		if (hasLowercase && hasUppercase && hasDigit && hasSpecial && length && !flag) {
+			System.out.println("Your password is secure");
+		}
+		
+		
+		if (counter != 0) {
+			System.out.println(s + " has " + counter + " requirements not met");
+		}	
+				
 		System.out.println("");
+		
 		return counter;
 	    }
 }
